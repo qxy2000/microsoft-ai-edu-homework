4ed4eaac2b3941bb69616e3644d80eb771aed114
qxy2000 <44153276+qxy2000@users.noreply.github.com>
2019-11-21 00:57:11 +0800

    Update boston_housing.py

--- boston/boston_housing.py
+++ boston/boston_housing.py
@@ -37,32 +37,32 @@ X_train,x_test,y_train,y_true = train_test_split(train,target,test_size=0.2)
 
 # In[3]:
 
-
+#创建学习模型
 linear = LinearRegression()
 
 
 # In[10]:
 
-
+#训练模型
 linear.fit(X_train,y_train)
 
 
 # In[5]:
 
-
+#预测数据
 y_pre_linear = linear.predict(x_test)
 
 
 # In[11]:
 
-
+#评分
 linear_score=r2_score(y_true,y_pre_linear)
 display(linear_score)
 
 
 # In[9]:
 
-
+#可视化
 #Linear
 plt.plot(y_true,label='true')
 plt.plot(y_pre_linear,label='linear')
